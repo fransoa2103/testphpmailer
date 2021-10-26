aec26a4b7e436b69cd7775ab08f823636e02d269
fransoa2103 <73296404+fransoa2103@users.noreply.github.com>
2021-10-26 12:00:34 +0200

    init mailjet

--- mail.php
+++ mail.php
@@ -1,56 +1,27 @@
 <?php
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
-require 'src/Exception.php';
-require 'src/PHPMailer.php';
-require 'src/SMTP.php';
-
-//Load Composer's autoloader
-// require 'vendor/autoload.php';
-sendmail(htmlspecialchars($_POST['email']), htmlspecialchars($_POST['subject']), htmlspecialchars($_POST['message']), $_POST['password']);
-function sendmail($email, $subject, $message, $password){
-    //Create an instance; passing `true` enables exceptions
-    $mail = new PHPMailer(true);
-    try {
-        //Server settings
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-        $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'Francois Cbonnaire';                     //SMTP username
-        $mail->Password   = $password;                               //SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-        //Recipients
-        $mail->setFrom('fcbonnaire01@gmail.com');
-        $mail->addAddress($email);     //Add a recipient
-        
-        //Content
-        $mail->isHTML(true);                                  //Set email format to HTML
-        $mail->Subject = $subject;
-        $mail->Body    = $message;
-        // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-        
-        $mail->send();
-        echo 'Message has been sent';
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    }
-}
-// var_dump($_POST);
-// echo htmlspecialchars($_POST['email'])."</br>".htmlspecialchars($_POST['subject'])."</br>".htmlspecialchars($_POST['message'])."</br>".$_POST['password']."</br>";
-// die();
-
-// $mail->addAddress('ellen@example.com');               //Name is optional
-// $mail->addReplyTo('info@example.com', 'Information');
-// $mail->addCC('cc@example.com');
-// $mail->addBCC('bcc@example.com');
-
-//Attachments
-// $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-// $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+  require 'vendor/autoload.php';
+  use \Mailjet\Resources;
+  $mj = new \Mailjet\Client('3a7e55f449b347f74f2ec5150df34a00','a6e0c5fb088959ef1595f3dae1dbf866',true,['version' => 'v3.1']);
+  $body = [
+    'Messages' => [
+      [
+        'From' => [
+          'Email' => "fcbonnaire01@gmail.com",
+          'Name' => "François"
+        ],
+        'To' => [
+          [
+            'Email' => "fcbonnaire01@gmail.com",
+            'Name' => "François"
+          ]
+        ],
+        'Subject' => "Greetings from Mailjet.",
+        'TextPart' => "My first Mailjet email",
+        'HTMLPart' => "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!",
+        'CustomID' => "AppGettingStartedTest"
+      ]
+    ]
+  ];
+  $response = $mj->post(Resources::$Email, ['body' => $body]);
+  $response->success() && var_dump($response->getData());
+?>
