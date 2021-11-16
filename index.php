625483d03ceb5f08e6aae0c64d9c668272063b76
fransoa2103 <73296404+fransoa2103@users.noreply.github.com>
2021-11-16 08:49:32 +0100

    add mess error or success after send mail

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<?php
+    session_start();
+    if (isset($_SESSION['send_email_true'])){
+        if ($_SESSION['send_email_true'] == true){
+            echo "<h3 style='color: green';>votre message a été envoyé avec succès!<h3>";
+        }
+        else {
+            echo "<p style='color: red';>Echec à l'envoi. Merci de renouveler votre opération.</p>";
+        }
+        $_SESSION['send_email_true'] = false;
+    }
+    else {
+        echo "<p style='color: blue';>Bienvenue sur votre formulaire mail</p>";
+        $_SESSION['send_email_true'] = false;
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,7 +28,6 @@
         <div style="font-weigh: bold; font-size: 1.5rem;">email destinataire</br><input type="email" id="email" name="email" placeholder="email destinataire"></div>
         <div style="font-weigh: bold; font-size: 1.5rem;">Sujet</br><input type="text" id="sujet" name="subject" placeholder="sujet du message"></div>
         <div style="font-weigh: bold; font-size: 1.5rem;">Votre message</br><textarea id="message" name="message" rows="5" cols="50" placeholder="message"></textarea></div>
-        <div style="font-weigh: bold; font-size: 1.5rem;">password</br><input type="password" id="password" name="password" placeholder="mdp de l'expediteur"></div>
         <div><input type="submit" value="submit"></div>
     </form>
 </body>
--- mail.php
+++ mail.php
@@ -1,32 +1,31 @@
 <?php
-  require 'vendor/autoload.php';
-  use \Mailjet\Resources;
-  $mj = new \Mailjet\Client('3a7e55f449b347f74f2ec5150df34a00','a6e0c5fb088959ef1595f3dae1dbf866',true,['version' => 'v3.1']);
-  $body = [
-    'Messages' => [
-      [
-        'From' => [
-          'Email' => "fcbonnaire01@gmail.com",
-          'Name' => "François CB"
-        ],
-        'To' => [
-          [
-            'Email' => htmlspecialchars($_POST['email']),
-            'Name' => "fucking test "
-          ]
-        ],
-        'Subject' => htmlspecialchars($_POST['subject']),
-        'TextPart' => "test email",
-        'HTMLPart' => htmlspecialchars($_POST['message'])
-        // 'CustomID' => "AppGettingStartedTest"
-      ]
-    ]
-  ];
-//   var_dump($body);
-//   print_r($body['Messages'][0]['To'][0]['Email'])."</br>";
-//   print_r($body['Messages'][0]['Subject'])."</br>";
-//   print_r($body['Messages'][0]['HTMLPart'])."</br>";
-  
-  $response = $mj->post(Resources::$Email, ['body' => $body]);
-  $response->success() && var_dump($response->getData());
+    session_start();
+    require 'vendor/autoload.php';
+    use \Mailjet\Resources;
+    $mj = new \Mailjet\Client('3a7e55f449b347f74f2ec5150df34a00','a6e0c5fb088959ef1595f3dae1dbf866',true,['version' => 'v3.1']);
+    $body = [
+        'Messages' => [
+            [
+                'From' => [
+                    'Email' => "fcbonnaire01@gmail.com",
+                    'Name' => "François CB"
+                ],
+                'To' => [
+                    [
+                        'Email' => htmlspecialchars($_POST['email']),
+                        'Name' => "fucking test "
+                    ]
+                ],
+                'Subject' => htmlspecialchars($_POST['subject']),
+                'TextPart' => "test email",
+                'HTMLPart' => htmlspecialchars($_POST['message'])
+            ]
+        ]
+    ];
+
+    $response = $mj->post(Resources::$Email, ['body' => $body]);
+    //   $response->success() && var_dump($response->getData());
+    $_SESSION['send_email_true'] = $response->success();
+    header('location: index.php');
+    exit();
 ?>
